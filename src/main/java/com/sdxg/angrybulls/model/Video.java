d694faa69cbe675335e5db8a5a0cfcd2e2a1b53b
syang <dcc1@qq.com>
2018-09-08 09:58:16 +0800

    增加视频播放量字段

--- src/main/java/com/sdxg/angrybulls/model/Video.java
+++ src/main/java/com/sdxg/angrybulls/model/Video.java
@@ -19,73 +19,54 @@ public class Video {
 
     private BigDecimal cost;
 
-    /**
-     * @return id
-     */
+    private Integer playAmount;
+
+
     public Integer getId() {
         return id;
     }
 
-    /**
-     * @param id
-     */
     public void setId(Integer id) {
         this.id = id;
     }
 
-    /**
-     * @return author
-     */
     public Integer getAuthor() {
         return author;
     }
 
-    /**
-     * @param author
-     */
     public void setAuthor(Integer author) {
         this.author = author;
     }
 
-    /**
-     * @return put_time
-     */
     public Date getPutTime() {
         return putTime;
     }
 
-    /**
-     * @param putTime
-     */
     public void setPutTime(Date putTime) {
         this.putTime = putTime;
     }
 
-    /**
-     * @return url
-     */
     public String getUrl() {
         return url;
     }
 
-    /**
-     * @param url
-     */
     public void setUrl(String url) {
         this.url = url;
     }
 
-    /**
-     * @return cost
-     */
     public BigDecimal getCost() {
         return cost;
     }
 
-    /**
-     * @param cost
-     */
     public void setCost(BigDecimal cost) {
         this.cost = cost;
+    }
+
+    public Integer getPlayAmount() {
+        return playAmount;
+    }
+
+    public void setPlayAmount(Integer playAmount) {
+        this.playAmount = playAmount;
     }
 }
